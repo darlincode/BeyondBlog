eb19a7cc93a51351d5ef7f0dd58f8a6f83e92ad7
nwannachukwuemekad@gmail.com <nwannachukwuemekad@gmail.com>
2021-06-07 09:57:44 +0100

    beyond-blog

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
+        BeyondBlog
+      </router-link>
+      <span class="attribution">
+        An interactive Blog from
+        <a
+          rel="noopener noreferrer"
+          target="blank"
+          href="https://github.com/darlincode"
+          >Dcchuks</a
+        >. Code &amp; design licensed under BeyondBlog
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
